22ef2840e91fed6625d883d24b2722a828300ee5
Peyman Shomalzadeh <peyman934@gmail.com>
2021-11-02 13:05:11 +0330

    add formModel to create Article

--- project/articles/models.py
+++ project/articles/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Article(models.Model):
     title = models.TextField()
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- project/articles/views.py
+++ project/articles/views.py
@@ -3,17 +3,31 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from articles.models import Article
 from django.contrib.auth.decorators import login_required
+from articles.forms import ArticleForm
 # Create your views here.
+
+# @login_required
+# def article_create_view(request):
+#     context = {"form": ArticleForm}
+#     form = ArticleForm(request.POST or None)
+#     context['form'] = form
+#     if form.is_valid():
+#         title = form.cleaned_data.get('title')
+#         content = form.cleaned_data.get('content')
+#         print(title,content)
+#         article_object = Article.objects.create(title=title,content=content)
+#         context['object'] = article_object
+#         context['created'] = True
+#     return render(request,"articles/create.html",context=context)
 
 @login_required
 def article_create_view(request):
-    context = {}
-    if request.method == "POST":
-        query_dict = request.POST
-        title = query_dict.get('title')
-        content = query_dict.get('content')
-        print(title,content)
-        article_object = Article.objects.create(title=title,content=content)
+    context = {"form": ArticleForm}
+    form = ArticleForm(request.POST or None)
+    context['form'] = form
+    if form.is_valid():
+        article_object = form.save()
+        context['form'] = ArticleForm()  # clean the form avoid post dublicate articles
         context['object'] = article_object
         context['created'] = True
     return render(request,"articles/create.html",context=context)

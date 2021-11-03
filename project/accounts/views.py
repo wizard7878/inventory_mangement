509a552c79cd291a2e4a7601fcd1541230d93075
Peyman Shomalzadeh <peyman934@gmail.com>
2021-11-03 12:23:35 +0330

    add register and login builtin form django

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 # Create your views here.
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request,username=username,password=password)
-        if(user is None):
-            context = {"Error":"Invalid username or password"}
-            return render(request,"accounts/login.html",context=context)
-        login(request,user)
-        return redirect("/")
-    return render(request,"accounts/login.html",{})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm(request)
+    context = {"form" : form}
+    return render(request,"accounts/login.html",context)
 
 def logout_view(request):
     if request.method == "POST":
@@ -21,5 +22,10 @@ def logout_view(request):
     return render(request,"accounts/logout.html",{})
 
 
-# def register_view(request):
-#     return render(request,"accounts/register.html",{})
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {"form" : form}
+    return render(request,"accounts/register.html",context)
--- project/project/urls.py
+++ project/project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', home_view),
     path("login/",account_view.login_view),
     path("logout/",account_view.logout_view),
+    path("register/",account_view.register_view),
     path('articles/', article_view.article_search_view),
     path("articles/create/",article_view.article_create_view),
     path('articles/<int:id>/', article_view.article_detail_view),

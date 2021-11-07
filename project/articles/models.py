09f7d45b26081357c264f386656bf7f2a81865a4
Peyman Shomalzadeh <peyman934@gmail.com>
2021-11-07 12:52:59 +0330

    add signal for add slug

--- project/articles/models.py
+++ project/articles/models.py
@@ -1,12 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import pre_save,post_save
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True,null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateTimeField(auto_now_add=False,auto_now=False,null=True,blank=True)
 
-    def __str__(self):
-        return self.title
+
+def article_pre_save(sender, instance ,*args,**kwargs):
+    print("Pre Save")
+    print(args,kwargs)
+    if instance.slug is None:
+        instance.slug = slugify(instance.title)
+
+
+pre_save.connect(article_pre_save,sender=Article)
+
+
+def article_post_save(sender, instance , created,*args,**kwargs):
+    print("Post Save")
+    print(args,kwargs)
+    if created:
+        instance.slug = slugify(instance.title)
+        instance.save()
+
+
+post_save.connect(article_post_save,sender=Article)
